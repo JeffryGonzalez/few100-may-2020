a6c60f237937cb9730009d28a4ae763674b825c8
Jeffry Gonzalez <jeff@hypertheory.com>
2020-05-18 14:41:55 -0400

    monday aftenoon

---
+++ spec/00_types.spec.ts
@@ -0,0 +1,11 @@
+describe('types in typescript', () => {
+    describe('declaring variables and constants', () => {
+
+        it('implicity any', () => {
+            // No Typescript in this example.
+
+        });
+    });
+
+});
+
