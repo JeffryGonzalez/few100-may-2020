01369a47eb8cecfc7dfa66b33d67bb15d9812b2e
Jeffry Gonzalez <jeff@hypertheory.com>
2020-05-20 11:33:06 -0400

    before lunch

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+
+const amountEl = document.getElementById('amount') as HTMLInputElement;
+const btnCalculatEl = document.getElementById('calculate') as HTMLButtonElement;
+
+btnCalculatEl.addEventListener('click', handleClick);
+
+
+function handleClick() {
+    const billAmount = amountEl.valueAsNumber;
+    console.log(`The Bill is for ${billAmount}`);
+}
 
